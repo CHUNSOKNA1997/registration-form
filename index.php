008ce5546ab54900ff450c3e3b40a15392d8986e
CHUNSOKNA1997 <chun.sokna.1222@gmail.com>
2025-03-01 15:00:25 +0700

    finished reg_form and made it interact with database

--- index.php
+++ index.php
@@ -10,11 +10,14 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="POST" class="h-screen w-screen flex flex-col justify-center items-center bg-blue-400">
-        <div class="flex flex-col w-[500px] h-auto items-center justify-center bg-white shadow-md space-y-12 px-6 py-8 rounded-lg">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="h-screen w-screen flex flex-col justify-center items-center bg-blue-400">
+        <div class="flex flex-col w-[500px] h-auto items-center justify-center bg-white shadow-md space-y-6 px-6 py-8 rounded-lg">
             <h1 class="text-3xl font-bold">Welcome to Registration Form</h1>
-                <input type="text" name="name" class="py-4 px-7 w-full border border-gray-700 rounded-full focus:outline-none text-xl font-semibold placeholder:text-base placeholder:font-normal" placeholder="username">
-                <input type="password" name="name" class="py-4 px-7 w-full border border-gray-700 rounded-full focus:outline-none placeholder:text-base placeholder:font-normal" placeholder="password">
+            
+            <input required type="text" name="username" class="py-4 px-7 w-full border border-gray-700 rounded-full focus:outline-none text-xl font-semibold placeholder:text-base placeholder:font-normal" placeholder="Username">
+            
+            <input required type="password" name="password" class="py-4 px-7 w-full border border-gray-700 rounded-full focus:outline-none text-xl font-semibold placeholder:text-base placeholder:font-normal" placeholder="Password">
+            
             <button type="submit" class="py-3 w-full bg-blue-600 hover:bg-blue-400 transition-colors duration-300 rounded-full text-white">Register</button>
         </div>
     </form>
@@ -22,4 +25,29 @@
 </html>
 
 <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if (empty($username) && empty($password)) {
+            echo "Please enter username and password!";
+        } elseif (empty($username)) {
+            echo "Please enter username!";
+        } elseif (empty($password)) {
+            echo "Please enter password!";
+        } else {
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+            $sql = "INSERT INTO tbrigistration (name, password) VALUES ('$username', '$hash')";
+
+            try{
+                mysqli_query($conn, $sql);
+            } catch(mysqli_sql_exception){
+                echo "This username is taken!";
+            }
+    }
+
     mysqli_close($conn);
+    }
+?>
