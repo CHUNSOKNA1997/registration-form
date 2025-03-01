1245fdb71b41298ccb92043b99973dc04f5b1f9b
CHUNSOKNA1997 <chun.sokna.1222@gmail.com>
2025-03-01 13:50:42 +0700

    added tailwind cnd to project and create form register

--- index.php
+++ index.php
@@ -6,12 +6,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./src/output.css">
     <title>Registration Form</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <form action="index.php" method="POST">
-        
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="POST" class="h-screen w-screen flex flex-col justify-center items-center">
+        <h1 class="text-xl">Welcome to Registration Form</h1>
+
+        <label for="username">Name:</label>
+        <input type="text" name="name">
+
+        <label for="password">Password:</label>
+        <input type="password" name="name">
+
+        <button type="submit">Register</button>
     </form>
 </body>
 </html>
